3026a3b0ad9de5ac27c8cae247d68e0e41695530
Yan Trindade <trindade.yan@gmail.com>
2017-09-15 15:22:06 -0300

    new first commit setup

---
+++ app/src/main/java/br/com/yimobile/igor/LoginActivity.java
@@ -0,0 +1,43 @@
+package br.com.yimobile.igor;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder builder;
+    private LayoutInflater layoutInflater;
+
+    private TextView registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerButton =  (TextView) findViewById(R.id.login_register);
+
+        registerButton.setOnClickListener(registerClickListener);
+    }
+
+    View.OnClickListener registerClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+
+//            builder = new AlertDialog.Builder(LoginActivity.this);
+//
+//            layoutInflater = getLayoutInflater();
+//            builder.setView(layoutInflater.inflate(R.layout.dialog_signup, null));
+//
+//            builder.create();
+//            builder.show();
+
+        }
+    };
+}
--- app/src/main/java/br/com/yimobile/igor/RegisterActivity.java
+++ app/src/main/java/br/com/yimobile/igor/RegisterActivity.java
@@ -1,8 +1,14 @@
 package br.com.yimobile.igor;
 
-/**
- * Created by brunovsiq on 15/09/17.
- */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class RegisterActivity {
+public class RegisterActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_signup);
+    }
 }
