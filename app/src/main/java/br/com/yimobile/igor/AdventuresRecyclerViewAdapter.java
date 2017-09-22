e9352d51c3abfeb49d1c5c1db8df79079d201c0a
Yan Trindade <trindade.yan@gmail.com>
2017-09-22 14:20:12 -0300

    add navdrawer and recyclerview

---
+++ app/src/main/java/br/com/yimobile/igor/AdventuresRecyclerViewAdapter.java
@@ -0,0 +1,47 @@
+package br.com.yimobile.igor;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class AdventuresRecyclerViewAdapter extends RecyclerView.Adapter {
+
+    private ArrayList<String> adventuresArrayList;
+    private Context context;
+
+
+    public AdventuresRecyclerViewAdapter(ArrayList<String> adventures, Context context) {
+        this.adventuresArrayList = adventures;
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.adventures_list_item, parent, false);
+
+        AdventuresViewHolder holder = new AdventuresViewHolder(view, context);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        AdventuresViewHolder adventureHolder = (AdventuresViewHolder) holder;
+
+        String title  = adventuresArrayList.get(position);
+
+        adventureHolder.titleTextView.setText(title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return adventuresArrayList.size();
+    }
+}
--- app/src/main/java/br/com/yimobile/igor/HomeActivity.java
+++ app/src/main/java/br/com/yimobile/igor/HomeActivity.java
@@ -1,17 +1,45 @@
 package br.com.yimobile.igor;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity{
 
+    Drawer result;
+    AdventuresRecyclerViewAdapter adventuresRecyclerViewAdapter;
+    RecyclerView recyclerView;
+    ArrayList<String> adventuresArrayList = new ArrayList<>();
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.loginatention);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.adventures_recyclerview);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adventuresArrayList.add("Teste 1");
+        adventuresArrayList.add("Teste 2");
+        adventuresArrayList.add("Teste 3");
+        adventuresRecyclerViewAdapter = new AdventuresRecyclerViewAdapter(adventuresArrayList, this);
+        recyclerView.setAdapter(adventuresRecyclerViewAdapter);
+
+        result = new DrawerBuilder().withActivity(this).build();
     }
 
     @Override
@@ -19,5 +47,16 @@ public class HomeActivity extends AppCompatActivity{
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                result.openDrawer();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
