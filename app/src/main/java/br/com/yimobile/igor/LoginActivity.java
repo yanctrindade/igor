5097c834ec80a6ac2ab13262b4937cf85e091da7
Marcos Paulo Cayres Rosa <mp_cayres96@hotmail.com>
2017-09-25 22:11:07 -0300

    Firebase + Login por Facebook
    +Adaptações no layout da tela de login

--- app/src/main/java/br/com/yimobile/igor/LoginActivity.java
+++ app/src/main/java/br/com/yimobile/igor/LoginActivity.java
@@ -1,19 +1,35 @@
 package br.com.yimobile.igor;
 
 import android.content.Intent;
-import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private AlertDialog.Builder builder;
-    private LayoutInflater layoutInflater;
+    private CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
 
     private TextView registerButton;
     private ImageButton enterButton;
@@ -23,6 +39,31 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_face);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("LOGIN", "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("CANCEL", "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("ERROR", "facebook:onError", error);
+            }
+        });
+
         registerButton =  (TextView) findViewById(R.id.criarconta);
         registerButton.setOnClickListener(registerClickListener);
 
@@ -30,6 +71,52 @@ public class LoginActivity extends AppCompatActivity {
         enterButton = (ImageButton) findViewById(R.id.enterButton);
         enterButton.setOnClickListener(enterButtonClickListener);
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("HANDLE_FACE", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SIGN_IN_SUCCESS", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SIGN_IN_FAIL", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user){
+        if(user != null){
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        }
     }
 
     View.OnClickListener registerClickListener = new View.OnClickListener() {
@@ -47,10 +134,29 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     };
+
     View.OnClickListener enterButtonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             startActivity(new Intent(LoginActivity.this, HomeActivity.class));
         }
     };
+
+    // Codigo para gerar chave para o facebook, caso necessario
+    /*private void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.i("KeyHash:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("jk", "Exception(NameNotFoundException) : " + e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("mkm", "Exception(NoSuchAlgorithmException) : " + e);
+        }
+    }*/
 }
