eef2223a7845d990f4c2acf3b5d52b0546910e0b
Yan Trindade <trindade.yan@gmail.com>
2017-11-10 14:49:02 -0200

    Push Notification Global Working

---
+++ app/src/main/java/br/com/yimobile/igor/screens/network/push/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package br.com.yimobile.igor.screens.network.push;
+
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+}
