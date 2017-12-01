5d64ee9fe8a8165f95ee4b89d19d9662a5afe8fe
Rennê Ruan <140030930@aluno.unb.br>
2017-12-01 10:50:31 -0200

    Base para Progress

--- app/src/main/java/br/com/yimobile/igor/screens/container/adventures/AdventuresRecyclerViewAdapter.java
+++ app/src/main/java/br/com/yimobile/igor/screens/container/adventures/AdventuresRecyclerViewAdapter.java
@@ -7,10 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 
 import br.com.yimobile.igor.R;
 import database.Adventure;
+import database.Session;
 
 public class AdventuresRecyclerViewAdapter extends RecyclerView.Adapter {
 
@@ -34,6 +41,7 @@ public class AdventuresRecyclerViewAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
         AdventuresViewHolder adventureHolder = (AdventuresViewHolder) holder;
+        ArrayList<Session> sessoes = (ArrayList<Session>) adventuresArrayList.get(position).getSessoes();
 
         String title  = adventuresArrayList.get(position).getNome();
         String descr  = adventuresArrayList.get(position).getDescricao();
@@ -41,6 +49,46 @@ public class AdventuresRecyclerViewAdapter extends RecyclerView.Adapter {
         adventureHolder.titleTextView.setText(title);
         adventureHolder.subtitleTextView.setText(descr);
 
+        if(sessoes != null && adventuresArrayList.get(position).getData() != null && sessoes.size() > 0) {
+            Collections.sort(sessoes, new Comparator<Session>() {
+                public int compare(Session s1, Session s2) {
+                    Date d1 = null, d2 = null;
+                    DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                    try {
+                        d1 = formatter.parse(s1.getData());
+                        d2 = formatter.parse(s2.getData());
+                    } catch (java.text.ParseException ie) {
+                        ie.printStackTrace();
+                    }
+                    return d1.compareTo(d2);
+                }
+            });
+
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            Date currentTime = Calendar.getInstance().getTime();
+            Date d1 = null, d2 = null;
+
+            try {
+                d1 = formatter.parse(adventuresArrayList.get(position).getData());
+                d2 = formatter.parse(sessoes.get(0).getData());
+
+            } catch (java.text.ParseException e) {
+                e.printStackTrace();
+            }
+
+            long diferencaInicioFim = d2.getTime() - d1.getTime();
+            long diferencaHojeInicio = currentTime.getTime() - d1.getTime();
+
+            float porcentagem;
+            if(diferencaInicioFim > 0 && diferencaHojeInicio < diferencaInicioFim) {
+                porcentagem = diferencaHojeInicio / diferencaInicioFim;
+            }else{
+                porcentagem = 1;
+            }
+
+            adventureHolder.progresso.setMax(100);
+            adventureHolder.progresso.setProgress((int)(porcentagem * 100));
+        }
         switch (adventuresArrayList.get(position).getBackground()){
             case 0:
                 adventureHolder.backgroundImageView.setImageResource(R.drawable.adv_bg_1);
